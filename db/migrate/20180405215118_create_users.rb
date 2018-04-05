e9660231c9d0543fe9e48bb0418b5a01512d65a9
Anna Royer <anna.royer@colorado.edu>
2018-04-05 16:23:21 -0600

    add user model to database

---
+++ db/migrate/20180405215118_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :uid
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :token
+      t.integer :oauth_expires_at
+
+      t.timestamps
+    end
+  end
+end

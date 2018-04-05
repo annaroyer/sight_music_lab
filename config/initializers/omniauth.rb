94c56a88f86ad7df4ce90baab9cb1972be5b933c
Anna Royer <anna.royer@colorado.edu>
2018-04-05 16:25:03 -0600

    add initializer for google omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
+    :name => "google",
+    :prompt => 'select_account',
+    :image_aspect_ratio => 'square',
+    :image_size => 50,
+    :access_type => 'offline'
+  }
+end

d85d00ada6ecea66aacbe78358eea181225a40d3
Anna Royer <anna.royer@colorado.edu>
2018-04-05 08:33:44 -0600

    add features spec directory and feature spec for user sees the song they recorded

---
+++ spec/features/user_sees_their_song_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  context 'when I upload audio and click generate' do
+    scenario 'I am on a song show page and I can see that song' do
+      visit songs_path
+
+      within('.vex-tabdiv') do
+        expect(page).to have_content('key=Bm time=4/4')
+        expect(page).to have_content('notes  C-D-E/4 F#/5 | G-A-B/4 F#/5')
+      end
+    end
+  end
+end

4ca15568bb918a4f65e4d952c88178ad1bf9985b
Anna Royer <anna.royer@colorado.edu>
2018-04-05 16:24:46 -0600

    add a spec that a visitor can log in using google oauth

--- spec/features/visitor_can_log_in_spec.rb
+++ spec/features/visitor_can_log_in_spec.rb
@@ -1,11 +1,35 @@
 require 'rails_helper'
 
-describe 'As a visitor' do
-  context 'when I visit / and click get started' do
-    scenario 'I can create an account through google' do
+describe 'As a user' do
+  context 'when I visit / and click login' do
+    scenario 'I can login using google' do
+      stub_omniauth
+
       visit root_path
 
-      click_on 'Get Started'
+      click_button 'Log In'
+
+      expect(current_path).to eq('/dashboard')
     end
   end
+end
+
+
+def stub_omniauth
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new({
+    provider: "google",
+    uid: "104267037309165833746",
+    info: {
+      name: "Anna Royer",
+      email: "anro0672@colorado.edu",
+      first_name: "Anna",
+      last_name: "Royer"
+    },
+    credentials: {
+      token: "ya29.GluUBTfiZVBGSyQoN6NarV53y8jFL47tyksZOMJ-2lcLDX2XbugaZGEZhnCrpHehwOijeM0VjpWZ1AZQLBbchkp6ErtR36esgmh71i5KZRR4etR2d3b74VZS6H34",
+      expires_at: 1522968194,
+      expires: true
+    }
+    })
 end

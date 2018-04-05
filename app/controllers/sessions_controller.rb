c5618384501505f9c8deb5f855e25c60f842dd22
Anna Royer <anna.royer@colorado.edu>
2018-04-05 16:47:02 -0600

    change session to use a users uid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,11 @@
 class SessionsController < ApplicationController
   def create
     user = User.update_or_create(request.env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to dashboard_path
+    session[:user_id] = user.uid
+    if user.save
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
   end
 end

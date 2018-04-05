f57b999e5417f92723ca04b629e2150bece9c8c9
Anna Royer <anna.royer@colorado.edu>
2018-04-05 17:21:10 -0600

    add logout rout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   get 'auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#index'
 end

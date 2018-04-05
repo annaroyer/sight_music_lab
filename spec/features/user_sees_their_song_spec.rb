45b039027acbe3794f2588cb84975bcc884f0804
Anna Royer <anna.royer@colorado.edu>
2018-04-05 11:07:09 -0600

    include notation test

--- spec/features/user_sees_their_song_spec.rb
+++ spec/features/user_sees_their_song_spec.rb
@@ -6,7 +6,7 @@ describe 'As a user' do
       visit songs_path
 
       within('.vex-tabdiv') do
-        expect(page).to have_content('key=Bm time=4/4')
+        expect(page).to have_content('key=Gm time=4/4')
         expect(page).to have_content('notes  C-D-E/4 F#/5 | G-A-B/4 F#/5')
       end
     end

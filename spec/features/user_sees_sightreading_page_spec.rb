d007ccc2b5312dbffc0233f1d8598e2ea05abf2a
Anna Royer <anna.royer@colorado.edu>
2018-04-09 16:54:46 -0600

    add to user sight read interaction

--- spec/features/user_sees_sightreading_page_spec.rb
+++ spec/features/user_sees_sightreading_page_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 describe 'As a user' do
   context 'when I visit /sight-read' do
-    scenario 'I see a sightreading example' do
+    before(:each) do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    end
 
+    scenario 'I see a sightreading example' do
       visit '/sight-read'
 
       expect(page).to have_css('.exercises')
       expect(page).to have_content('120 bpm')
+    end
+
+    scenario 'I can upload an audio file' do
+      visit '/sight-read'
+
+      click_on 'Upload File'
     end
   end
 end

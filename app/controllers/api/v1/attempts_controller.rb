b671af68bad59ccd0b7882d049f8c4cf652f7922
Anna Royer <anna.royer@colorado.edu>
2018-04-10 08:52:23 -0600

    add private methods for attempt_params

--- app/controllers/api/v1/attempts_controller.rb
+++ app/controllers/api/v1/attempts_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::AttemptsController < ApiController
   def create
-    attempt = current_user.attempts.create(params[:melody_result])
+    attempt = current_user.attempts.create(attempt_params)
     render json: attempt
   end
+
+  private
+    def attempt_params
+      {score: 5, song: song}
+    end
+
+    def song
+      Song.new(params[:melody_result])
+    end
 end

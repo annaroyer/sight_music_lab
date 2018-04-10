41706ff3f6cce6924f28a7ef24a863c28de92a28
Anna Royer <anna.royer@colorado.edu>
2018-04-10 15:49:19 -0600

    add private method to create a song and create an attempt with a song

--- app/controllers/api/v1/attempts_controller.rb
+++ app/controllers/api/v1/attempts_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::AttemptsController < ApiController
   def create
-    attempt = current_user.attempts.create(attempt_params)
+    attempt = current_user.attempts.create(song: song)
+    binding.pry
     render json: attempt
   end
 
   private
-    def attempt_params
-      {score: 5, song: song}
-    end
-
     def song
       Song.new(params[:melody_result])
     end

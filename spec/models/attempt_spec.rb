93b9440a6c49de437cfe4a5153a585b2ef33acd4
Anna Royer <anna.royer@colorado.edu>
2018-04-10 15:56:14 -0600

    update to match json coming in

--- spec/models/attempt_spec.rb
+++ spec/models/attempt_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Attempt, type: :model do
   context 'validations' do
     it { should validate_presence_of :song }
-    it { should validate_presence_of :score }
   end
 
   context 'relationships' do

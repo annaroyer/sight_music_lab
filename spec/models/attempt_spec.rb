d0dd9c8c0ed64c1725a58b7e247887603eb0e136
Anna Royer <anna.royer@colorado.edu>
2018-04-10 08:52:42 -0600

    add validations and relationship tests for attempt

--- spec/models/attempt_spec.rb
+++ spec/models/attempt_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 describe Attempt, type: :model do
-  
+  context 'validations' do
+    it { should validate_presence_of :song }
+    it { should validate_presence_of :score }
+  end
+
+  context 'relationships' do
+    it { should belong_to :user }
+  end
 end

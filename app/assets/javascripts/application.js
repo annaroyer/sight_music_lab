d8cf347e803305cd2504abb0023139356a428ece
Anna Royer <anna.royer@colorado.edu>
2018-04-11 17:09:59 -0600

    update assets load order and add libraries to javascripts for heroku loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,33 @@ $(document).ready(function(){
     .then(response => $.ajax({
         method: 'POST',
         url: 'api/v1/attempts',
-        data: response
-      }))
+        data: response,
+        success: function(data) {
+          var notes = data.song.notes.join(' ');
+          var exercises = document.getElementById('exercises');
+          exercises.lastElementChild.append('notes ' + notes + ' =||');
+        }
+      }));
     })
   });
+
+
+  VF = Vex.Flow;
+
+  // Create an SVG renderer and attach it to the DIV element named "boo".
+  var div = document.getElementById("score")
+  var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+  // Configure the rendering context.
+  renderer.resize(500, 500);
+  var context = renderer.getContext();
+  context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+  // Create a stave of width 400 at position 10, 40 on the canvas.
+  var stave = new VF.Stave(10, 40, 400);
+
+  // Add a clef and time signature.
+  stave.addClef("treble").addTimeSignature("4/4");
+
+  // Connect it to the rendering context and draw!
+  stave.setContext(context).draw();

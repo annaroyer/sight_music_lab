40df5517fcabb49434fb9700933a669e873cd822
Anna Royer <anna.royer@colorado.edu>
2018-04-12 14:27:57 -0600

    add abcjs files to javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require rails-ujs
-//= require vexflow-min.js
-//= require tabdiv-debug.js
+//= require abcjs-rails
 //= require_tree .
+
+var abc = "T: Cooley's\n" +
+			"M: 4/4\n" +
+			"L: 1/8\n" +
+			"K: Emin\n" +
+			"|:d2|EBBA B2 EB|B2 AB dBAG|FDAD BDAD|FDAD dAFD|\n" +
+			"EBBA B2 EB|B2 AB defg|afe^c dBAF|DEFD E2:|\n" +
+			"|:gf|eB B2 efge|eB B2 gedB|A2 FA DAFA|A2 FA defg|\n" +
+			"eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|";
+
+ABCJS.renderAbc("paper", abc);
+// ABCJS.renderAbc(div, abcString, parserParams, printerParams, renderParams);
+// ABCJS.renderMidi(div, abcString, parserParams, midiParams, renderParams);

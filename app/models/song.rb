f88ec63d1b04313cec702b0d2c42de7a5d8f1c6a
Anna Royer <anna.royer@colorado.edu>
2018-04-12 17:29:37 -0600

    delete a couple unnecessary methods since switching to abcjs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,8 @@
 class Song
-  attr_reader :key, :tse, :bpm
+  attr_reader :key, :tse
 
   def initialize(attrs, tse='4/4', bpm=130)
-    @key = attrs[:key]
+    @key = attrs[:key].gsub('#', '^')
     @tse = tse
     @bpm = bpm
     @raw_notes = attrs[:notes].values
@@ -10,27 +10,23 @@ class Song
 
   def notes
     @notes ||= raw_notes.map do |raw_note|
-      note = Note.new(raw_note, single_beat_type, beat_duration)
+      note = Note.new(raw_note, beat_duration)
       note if note.audible?
     end.compact
   end
 
+  def beats_per_measure
+    @beats_per_measure ||= tse.split('/').first.to_i
+  end
+
+  def each_beat
+    '1/' + tse.split('/').last
+  end
+
   private
-    attr_reader :raw_notes
-
-    def beats_per_measure
-      @beats_per_measure ||= tse.split('/').first.to_i
-    end
-
-    def single_beat_type
-      @single_beat_type ||= tse.split('/').last.to_f
-    end
+    attr_reader :raw_notes, :bpm
 
     def beat_duration
       @beat_duration ||= 60.0 / bpm.to_f
-    end
-
-    def measure_duration
-      @measure_duration ||= beat_duration * beats_per_measure
     end
 end

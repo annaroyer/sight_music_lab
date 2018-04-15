509fd2caedd6b45a9429087faf32c533059cdc06
Anna Royer <anna.royer@colorado.edu>
2018-04-15 16:15:14 -0600

    add audio attachment to attempt model

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,6 +1,10 @@
 class Attempt < ApplicationRecord
   belongs_to :user
   validates_presence_of :song
-  
+
   serialize :song
+
+  attr_accessor :audio_file_name
+  has_attached_file :audio
+  validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio', 'audio/x-wav', 'audio/wav' ]
 end

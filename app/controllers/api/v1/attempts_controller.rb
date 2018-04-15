66d2b7b6b1a97e87883831af8d2fc04e74a59800
Anna Royer <anna.royer@colorado.edu>
2018-04-14 21:29:23 -0600

    add faraday file upload sequence to controller - will move out to service

--- app/controllers/api/v1/attempts_controller.rb
+++ app/controllers/api/v1/attempts_controller.rb
@@ -1,11 +1,34 @@
 class Api::V1::AttemptsController < ApiController
   def create
-    attempt = current_user.attempts.create(song: song)
-    render json: attempt
+    current_user = User.first
+    render json: current_user.attempts.create(song: song)
   end
 
   private
+
     def song
-      Song.new(params[:melody_result])
+      response = conn.post('', payload)
+      raw_song = JSON.parse(response.body, symbolize_names: true)[:melody_result]
+      Song.new(raw_song)
+    end
+
+    def input_file
+      Faraday::UploadIO.new(
+       params[:input_file].tempfile.path,
+       params[:input_file].content_type,
+       params[:input_file].original_filename
+      )
+    end
+
+    def payload
+      { input_file: input_file }
+    end
+
+    def conn
+      Faraday.new('https://api.sonicapi.com/analyze/melody?access_id=67a84cd6-5d82-4f3b-bf96-752496ab2670&format=json') do |f|
+       f.request :multipart
+       f.request :url_encoded
+       f.adapter Faraday.default_adapter
+      end
     end
 end

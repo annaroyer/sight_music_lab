929ddc36da817b3bb2e9c645af04fb31e53bbd30
Anna Royer <anna.royer@colorado.edu>
2018-04-16 14:55:32 -0600

    add mic to mp3 js library to static assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,59 +16,66 @@
 //= require_tree .
 //= require abcjs-rails
 
-  var player = document.getElementById('player');
+var originalExercise = "M: 4/4\n" +
+                       "L: 1/4\n" +
+                       "K: A^ Maj\n" +
+                       "A^1 |  D1 F1 D1 A^2||";
+ABCJS.renderAbc("exercise-score", originalExercise);
 
-  $('#recorder').on('change', function(e) {
-    var file = e.target.files[0];
+const recorder = new MicRecorder({
+  bitRate: 128
+});
+
+const player = $('#player');
+
+$('#upload').on('change', function(e) {
+  var file = e.target.files[0];
+  getSheetMusic(file);
+});
+
+$('#start').on('click', startRecording);
+
+function startRecording(){
+  recorder.start().then(() => {
+    $('#start').off();
+    $('#stop').on('click', stopRecording);
+  }).catch((e) => {
+    console.error(e);
+  });
+}
+
+function stopRecording(){
+  recorder.stop().getMp3().then(([buffer, blob]) => {
+    console.log(buffer, blob);
+    var file = new File(buffer, 'input.mp3', {
+      type: blob.type,
+      lastModified: Date.now()
+    });
+    $('#stop').off();
     getSheetMusic(file);
+  }).catch((e) => {
+    console.error(e);
   });
+}
 
-    var originalExercise = "M: 4/4\n" +
-                           "L: 1/4\n" +
-                           "K: A^ Maj\n" +
-                           "A^1 |  D1 F1 D1 A^2||";
-    ABCJS.renderAbc("exercise-score", originalExercise);
+const getSheetMusic = function(file){
+  player.src = URL.createObjectURL(file);
+  var formData = new FormData();
+  formData.append('attempt[audio]', file);
 
-
-navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-  .then(function(stream){
-    var mediaRecorder = new MediaRecorder(stream, {mimeType : "audio/webm"});
-    $('#start').on('click', function(){
-      mediaRecorder.start();
-    });
-    var sound_buffers = [];
-    mediaRecorder.ondataavailable = function(e) {
-      sound_buffers.push(e.data)
-    }
-    $('#stop').on('click', function(){
-      mediaRecorder.stop();
-    });
-    mediaRecorder.onstop = function(e) {
-      const blob = new Blob(sound_buffers, { type: 'audio/ogg' });
-      var file = new File(sound_buffers, 'input.webm', { type: 'audio/webm' });
-      sound_buffers = [];
-      getSheetMusic(blob);
-    }
-  });
-
-  const getSheetMusic = function(file){
-    player.src = URL.createObjectURL(file);
-    var formData = new FormData();
-    formData.append('attempt[audio]', file);
-
-    fetch('/api/v1/attempts', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => response.json())
-    .then(function(response){
-      var song = response.song;
-      var newAttempt = `M: ${song.tse}\n` +
-                        `L: ${song.each_beat}\n` +
-                        `K: ${song.key}\n` +
-                        `${song.notes.join(' ')}` + '||';
-      var attemptScore = $('.exercises').append($('<div>', {class: 'paper', id: 'attempt-score'}));
-      ABCJS.renderAbc('attempt-score', newAttempt);
-    })
-    .catch((error) => console.error('Error:', error))
-  }
+  fetch('/api/v1/attempts', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(function(response){
+    var song = response.song;
+    var newAttempt = `M: ${song.tse}\n` +
+                      `L: ${song.each_beat}\n` +
+                      `K: ${song.key}\n` +
+                      `${song.notes.join(' ')}` + '||';
+    var attemptScore = $('.exercises').append($('<div>', {class: 'paper', id: 'attempt-score'}));
+    ABCJS.renderAbc('attempt-score', newAttempt);
+  })
+  .catch((error) => console.error('Error:', error))
+}

5c6b53d456daf1d02152a096afc5173f7ae48db3
Anna Royer <anna.royer@colorado.edu>
2018-04-16 01:40:39 -0600

    change argument for song from_upload

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -2,16 +2,10 @@ class Attempt < ApplicationRecord
   belongs_to :user
 
   def song
-    Song.from_upload(audio)
+    Song.from_upload(audio.url)
   end
 
   attr_accessor :audio_file_name
   has_attached_file :audio
-  do_not_validate_attachment_file_type :audio,
-  storage: :s3,
-    s3_credentials: {
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    },
-      bucket: ENV['S3_BUCKET_NAME']
+  do_not_validate_attachment_file_type :audio
 end

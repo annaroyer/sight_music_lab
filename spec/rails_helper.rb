9c58f3cd9946ede63f1d98c1fdbf8850d759c7a5
Anna Royer <anna.royer@colorado.edu>
2018-04-17 14:29:21 -0600

    add database cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,6 +58,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  DatabaseCleaner.strategy = :truncation
+
+  config.before(:all) do
+    DatabaseCleaner.clean
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 Shoulda::Matchers.configure do |config|

f88822156e209c77304df125ac578572e1ab92a6
Anna Royer <anna.royer@colorado.edu>
2018-04-17 17:24:10 -0600

    move current_user method to api controller and update params for new param names

--- app/controllers/api/v1/exercises/attempts_controller.rb
+++ app/controllers/api/v1/exercises/attempts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Exercises::AttemptsController < ApiController
+
   def create
     render json: current_user.attempts.create(attempt_params)
   end
@@ -7,9 +8,5 @@ class Api::V1::Exercises::AttemptsController < ApiController
 
     def attempt_params
       params.permit(:exercise_id, :audio)
-    end
-
-    def current_user
-      User.find_by(email: params[:user_email])
     end
 end

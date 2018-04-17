ac86489dd6652e27c59b6522a83116d7fdf481a9
Anna Royer <anna.royer@colorado.edu>
2018-04-17 10:50:40 -0600

    remove require login method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,8 +5,3 @@ class ApiController < ActionController::API
     @current_user ||= User.find_by(uid: session[:user_id]) || Guest.new
   end
 end
-
-def require_login!
-  return true if authenticate_token
-  render json: { errors: [ { detail: "Access denied" } ] }, status: 401
-end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+  
   def create
     user = User.update_or_create(request.env["omniauth.auth"])
     session[:user_id] = user.uid

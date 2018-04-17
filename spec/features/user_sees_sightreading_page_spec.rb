fb2b6f039f1cc04e80ad3e2e25acacd39b89a23f
Anna Royer <anna.royer@colorado.edu>
2018-04-17 14:30:12 -0600

    test that the exercises are different when you visit the page different times

--- spec/features/user_sees_sightreading_page_spec.rb
+++ spec/features/user_sees_sightreading_page_spec.rb
@@ -12,8 +12,29 @@ describe 'As a user' do
 
       visit '/sight-read'
 
+      expect(page).to have_css('input')
+      expect(page).to have_css('audio')
+      expect(page).to have_button('Record')
       expect(page).to have_css('.exercises')
       expect(page).to have_css("##{exercise.id}")
+
+      within("##{exercise.id}") do
+        expect(page).to have_content(exercise.notes)
+        expect(page).to have_content("M: #{exercise.tse}")
+        expect(page).to have_content("K: #{exercise.key}")
+      end
+    end
+  end
+
+  scenario 'I see a different sightreading example' do
+    exercise = Exercise.create(key: 'C min', tse: '3/4', notes: "C1 E2 C1 | c2 b1 c1|]")
+
+    visit '/sight-read'
+
+    within("##{exercise.id}") do
+      expect(page).to have_content(exercise.notes)
+      expect(page).to have_content("M: #{exercise.tse}")
+      expect(page).to have_content("K: #{exercise.key}")
     end
   end
 end

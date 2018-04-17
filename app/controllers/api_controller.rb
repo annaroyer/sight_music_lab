0b02d3725d3ceca2775e69969cf928ebec55721c
Anna Royer <anna.royer@colorado.edu>
2018-04-17 17:25:11 -0600

    add current_user helper method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,7 @@
-class ApiController < ApplicationController
+class ApiController < ActionController::API
+  helper_method :current_user
 
+  def current_user
+    User.find_by(email: params[:user_email])
+  end
 end

83e44c726b82c1cc0d5b5bef3472b98c62c6250f
Anna Royer <anna.royer@colorado.edu>
2018-04-16 19:45:45 -0600

    add attempt factory with attached file

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,9 @@ FactoryBot.define do
     sequence(:token) { |n| "#{n}" }
     oauth_expires_at 123456
   end
+
+  factory :attempt do
+    user
+    audio { File.new("#{Rails.root}/spec/fixtures/recorded.mp3", 'w') }
+  end
 end

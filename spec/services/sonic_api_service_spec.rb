bb6ee642cae66ccc1185566e93c5c64cb2f1e8d8
Anna Royer <anna.royer@colorado.edu>
2018-04-16 23:10:45 -0600

    write spec for sonic_api_service using vcr to send request

---
+++ spec/services/sonic_api_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe SonicApiService do
+  context 'class methods' do
+    describe '.post_file' do
+      it 'returns the json from posting an audio file url to the sonic api' do
+        VCR.use_cassette('melody_result') do
+          url = "https://sight-music-lab.s3-us-west-1.amazonaws.com/attempts/audios/000/000/020/original/recorded.mp3"
+
+          melody_result = SonicApiService.post_file(url)
+
+          expect(melody_result[:key]).to eq("A# Maj")
+          expect(melody_result[:notes].count).to eq(13)
+          expect(melody_result[:notes].first[:midi_pitch]).to eq(58.236053466796875)
+        end
+      end
+    end
+  end
+end

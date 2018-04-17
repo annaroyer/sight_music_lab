46c44c9b261159abc34d5509f2ff38399778fce5
Anna Royer <anna.royer@colorado.edu>
2018-04-17 17:09:41 -0600

    change to find user and exercise in attempts controller

--- app/controllers/api/v1/exercises/attempts_controller.rb
+++ app/controllers/api/v1/exercises/attempts_controller.rb
@@ -1,11 +1,15 @@
-class Api::V1::Exercises::AttemptsController < ApplicationController
+class Api::V1::Exercises::AttemptsController < ApiController
   def create
-    render json: current_user.attempts.create(attempt_params)
+    render json: exercise.attempts.create(attempt_params)
   end
 
   private
 
     def attempt_params
-      params.require(:attempt).permit(:audio)
+      params.require(:attempt).permit(:audio, :user_email)
+    end
+
+    def exercise
+      Exercise.find(params[:exercise_id])
     end
 end

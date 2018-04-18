c88b7cf9cccfe3979cccf5f7f5a8f1ac446236ef
Anna Royer <anna.royer@colorado.edu>
2018-04-17 23:45:43 -0600

    add upload model just to hold reference to s3 recorded song

---
+++ app/models/upload.rb
@@ -0,0 +1,14 @@
+class Upload < ApplicationRecord
+  attr_accessor :audio_file_name
+  has_attached_file :audio
+  do_not_validate_attachment_file_type :audio,
+  storage: :s3,
+  s3_region: ENV.fetch('AWS_REGION'),
+  s3_host_name: 's3-us-west-1.amazonaws.com',
+  path: '/:class/:attachment/:id_partition/:style/:filename',
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+  }
+end

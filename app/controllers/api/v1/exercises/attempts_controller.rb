fa4bb079b265c6d8271e62647d63b8e8e9d366b7
Anna Royer <anna.royer@colorado.edu>
2018-04-17 23:47:16 -0600

    change controller to account for new attempts attributes - really fat controller will move out to service object (?)

--- app/controllers/api/v1/exercises/attempts_controller.rb
+++ app/controllers/api/v1/exercises/attempts_controller.rb
@@ -1,12 +1,48 @@
 class Api::V1::Exercises::AttemptsController < ApiController
 
   def create
-    render json: current_user.attempts.create(attempt_params)
+    render json: Attempt.create(song: song,
+                                exercise: exercise,
+                                rhythm_score: rhythm_score,
+                                pitch_score: pitch_score,
+                                user: current_user)
   end
 
   private
 
-    def attempt_params
-      params.permit(:exercise_id, :audio)
+    def current_user
+      User.find_by(email: params[:user_email])
+    end
+
+    def exercise
+      @exercise ||= Exercise.find(params[:exercise_id])
+    end
+
+    def comparison
+      @comparison ||= exercise.notes.gsub(' |', '').split(' ').zip(song.notes)
+    end
+
+    def upload
+      Upload.create(audio: params[:audio]).audio.url
+    end
+
+    def song
+      @song ||= Song.from_upload(upload, exercise.tse)
+    end
+
+    def rhythm_score
+      comparison.count do |notes|
+        notes.first.end_with?(notes.last.num_beats.to_s)
+      end * score_weight
+    end
+
+    def pitch_score
+      comparison.count do |notes|
+        notes.first.start_with?(notes.last.letter)
+      end * score_weight
+    end
+
+    def score_weight
+      @score_weight ||= 5 / comparison.count
     end
 end

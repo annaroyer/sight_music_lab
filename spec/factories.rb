a86f589568131a84db9d43d96b908e52be4c7aad
Anna Royer <anna.royer@colorado.edu>
2018-04-18 01:48:03 -0600

    remove attempt factory and update exercise factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,12 +8,6 @@ FactoryBot.define do
     oauth_expires_at 123456
   end
 
-  factory :attempt do
-    user
-    audio { File.new("#{Rails.root}/spec/fixtures/recorded.mp3", 'w') }
-    exercise
-  end
-
   factory :exercise do
     notes "z1 A^,1 D1 | F1 D1 A^,2"
     key 'A^ Maj'

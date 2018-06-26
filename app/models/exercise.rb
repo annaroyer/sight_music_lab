cdff03f8e72cbbbc6a3859cf52e00f2cdace1437
Anna Royer <anna.royer@colorado.edu>
2018-06-26 09:13:40 -0600

    add each_beat instance method to exercise and refactor class method random

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,7 +3,11 @@ class Exercise < ApplicationRecord
   has_many :attempts
 
   def self.random
-    offset = rand(count)
-    Exercise.offset(offset).first
+    random_id = rand(1..count)
+    find(random_id)
+  end
+
+  def single_beat
+    "1" + tse[tse.index('/')..-1]
   end
 end

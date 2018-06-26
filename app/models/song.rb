a531372f38263d18775beb01f4fcfece91c9aff9
Anna Royer <anna.royer@colorado.edu>
2018-06-26 09:12:09 -0600

    add full exercise object on initialize to song and change rhythm rendering

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,21 +1,21 @@
 class Song
   attr_reader :key, :tse
 
-  def initialize(attrs, tse='4/4', bpm=130)
-    @key = attrs[:key].gsub('#', '^')
-    @tse = tse
+  def initialize(raw_notes, exercise, bpm=130)
+    @key = exercise.key
+    @tse = exercise.tse
     @bpm = bpm
-    @raw_notes = attrs[:notes]
+    @raw_notes = raw_notes
   end
 
-  def self.from_upload(audio, tse='4/4', bpm=130)
-    raw_song = SonicApiService.post_file(audio)
-    new(raw_song, tse='4/4', bpm=130)
+  def self.from_upload(audio, exercise, bpm=130)
+    raw_notes = SonicApiService.post_file(audio)
+    new(raw_notes, exercise, bpm=130)
   end
 
   def notes
     @notes ||= raw_notes.map do |raw_note|
-      Note.new(raw_note, eighth_note_duration)
+      Note.new(raw_note, beat_duration)
     end
   end
 
@@ -28,10 +28,6 @@ class Song
 
     def beats_per_measure
       @beats_per_measure ||= tse.to_i
-    end
-
-    def eighth_note_duration
-      (tse[-1].to_f / 8) * beat_duration
     end
 
     def beat_duration

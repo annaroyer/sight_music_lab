61b7c3e3a4d7139158455b22d1f60a9122e6e40b
Anna Royer <anna.royer@colorado.edu>
2018-06-26 08:48:58 -0600

    change rhythm score expectation to fit factory exercise

--- spec/services/attempt_analyzer_spec.rb
+++ spec/services/attempt_analyzer_spec.rb
@@ -27,7 +27,7 @@ describe AttemptAnalyzer do
           expect(upload).to have_received(:audio)
           expect(song).to have_received(:notes)
           expect(attempt.pitch_score).to eq(2)
-          expect(attempt.rhythm_score).to eq(1)
+          expect(attempt.rhythm_score).to eq(4)
         end
       end
     end

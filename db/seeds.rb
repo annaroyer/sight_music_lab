c422e8b20a24fab26ace5bdbbc3635d75038c920
Anna Royer <anna.royer@colorado.edu>
2018-06-27 14:26:27 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Exercise.create(notes: "C1 E1 C1 | c1 B1 c1|]", key: "C min", tse: "3/4")
+Exercise.create(notes: "C2 D1C1 E2 G2 | A2 G1G1 A2 B2 | c2 G2 ^F2 G2 | A2 F1F1 E2 F2 | G2 E2 A4 |]", key: "C maj", tse: "4/4")

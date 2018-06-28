078bc83e228c8680c23bafa2a392aaa77cb539f2
Anna Royer <anna.royer@colorado.edu>
2018-06-28 05:19:38 -0600

    fix error in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Exercise.create(notes: "C1 E1 C1 | c1 B1 c1|]", key: "C min", tse: "3/4")
-Exercise.create(notes: "C1 D1/2C1/3 E1 G1 | A1 G1/2G1/2 A1 B1 | c1 G1 ^F1 G1 | A1 F1/2F1/2 E1 F1 | G1 E1 A2 |]", key: "C maj", tse: "4/4")
+Exercise.create(notes: "C1 D1/2C1/2 E1 G1 | A1 G1/2G1/2 A1 B1 | c1 G1 ^F1 G1 | A1 F1/2F1/2 E1 F1 | G1 E1 A2 |]", key: "C maj", tse: "4/4")

056adf3f01bf08e1c82f324e0998a93d31d85d22
Anna Royer <anna.royer@colorado.edu>
2018-06-27 23:22:41 -0600

    fix exercise notes in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Exercise.create(notes: "C1 E1 C1 | c1 B1 c1|]", key: "C min", tse: "3/4")
-Exercise.create(notes: "C2 D1C1 E2 G2 | A2 G1G1 A2 B2 | c2 G2 ^F2 G2 | A2 F1F1 E2 F2 | G2 E2 A4 |]", key: "C maj", tse: "4/4")
+Exercise.create(notes: "C1 D1/2C1/3 E1 G1 | A1 G1/2G1/2 A1 B1 | c1 G1 ^F1 G1 | A1 F1/2F1/2 E1 F1 | G1 E1 A2 |]", key: "C maj", tse: "4/4")
